ec25384d88ee6f1c8a774fbd5a745441f334c0bf
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-04-02 16:45:14 -0700

    Create a test harness for shared examples

---
+++ test/harness.js
@@ -0,0 +1,78 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the MIT license. Please see LICENSE file in the project root for terms.
+
+var request = require('supertest');
+var fixture = require('./fixture');
+
+/**
+ * Shared expectations for each version of the hash API.
+ */
+
+module.exports = function (app) {
+
+  it('hashes an http.IncomingMessage', function(done) {
+    request(app())
+    .get('/')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces a different hash for a different path', function(done) {
+    request(app())
+    .get('/quux')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces a different hash for a different query string', function (done) {
+    request(app())
+    .get('/?foo=1&bar=2')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces the same hash for different ordered query params', function (done) {
+    request(app())
+    .get('/?bar=2&foo=1')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces a different hash for a different method', function(done) {
+    request(app())
+    .post('/')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces a different hash for different headers', function(done) {
+    request(app())
+    .get('/')
+    .set('host', 'localhost:4567')
+    .set('x-foo', 'bar')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('produces a different hash for a different post body', function (done) {
+    request(app())
+    .post('/')
+    .set('host', 'localhost:4567')
+    .send('yay')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('can use a different hash algorithm', function(done) {
+    request(app('sha1'))
+    .get('/')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+  it('can use a different encoding', function(done) {
+    request(app('md5', 'base64'))
+    .get('/')
+    .set('host', 'localhost:4567')
+    .expect(fixture[this.test.title], done);
+  });
+
+};
