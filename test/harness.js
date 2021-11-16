d6e01853189fe49c2806cf7e87f676872156b939
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2021-11-15 17:37:40 -0800

    2021

--- test/harness.js
+++ test/harness.js
@@ -1,4 +1,4 @@
-// Copyright 2019 SmugMug, Inc.
+// Copyright 2021 SmugMug, Inc.
 // Licensed under the terms of the MIT license. Please see LICENSE file in the project root for terms.
 
 /* eslint-env mocha */
